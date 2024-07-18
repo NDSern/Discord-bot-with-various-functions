529819238514b26e6237532d948cd98cc9887b98
NDSern <potarau123@gmail.com>
2024-07-18 12:08:14 +0300

    Some commands that might help with game dev

--- bot.py
+++ bot.py
@@ -58,6 +58,13 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if message.content.startswith('/clap_help'):
+        list_of_commands = "**/translate <msg>**: Translate message\n"
+        list_of_commands += "**/leetd**: Give a Leetcode daily problem screenshot\n"
+        list_of_commands += "**/leetnewd**: Get a new Leetcode daily screenshot, in case /leetd did not update for the new daily challenge\n"
+        list_of_commands += "**/leetsubscribe**: Subscribe channel to be notified everytime there is a new Leetcode daily\n"
+        await message.channel.send(list_of_commands)
 
     if message.content.startswith('/rand'):
         return
@@ -105,5 +112,100 @@ async def on_message(message):
             members += "\n"
         smessage = "There are " + str(guild.member_count) + ". They are: " + members
         await message.channel.send(smessage)
+        
+    #Game Jam stuffs
+    if message.content.startswith("/gamejam"):
+        gamejaminfo = "[Pirate Software Game Jam](<https://itch.io/jam/pirate>)\n"
+        gamejaminfo += "Theme to follow: Shadow & Alchemy\n"
+        gamejaminfo += "[Repo](<https://github.com/NDSern/Game-Jam-7-2024>)\n"
+        gamejaminfo += "[Docs](<https://drive.google.com/drive/folders/133YJRN0rvEuacpPovOMlSTYIQs4L3oKY?usp=sharing>)\n"
+        gamejaminfo += "Current project: Plague Doctor from DD have a shadow cloak and go melee people\n"
+        gamejaminfo += "Engine: Godot\n"
+        gamejaminfo += "Art & Sound software: Haven't decided yet\n"
+        await message.edit(suppress=True)
+        await message.channel.send(gamejaminfo)
+    
+    if message.content.startswith("/jam_help"):
+        commands = "**/todos**: Show the list of things to do\n"
+        commands += "**/past**: Show the list of things that have been done\n"
+        commands += "**/addtd <str>**: Add a new task\n"
+        commands += "**/do <int>**: Mark yourself as doing something\n"
+        commands += "**/done <int>**: Mark yourself as completed a task\n"
+        await message.channel.send(commands)
+    
+    if message.content.startswith("/todos"):
+        todo_list = ""
+        try:
+            with open('todos.txt', 'r') as f:
+                for i, line  in enumerate(f):
+                    todo_list += str(i) + ". " + line
+        except FileNotFoundError:
+            print("No such file")
+        if not todo_list:
+            todo_list = "Everything is done, for now."
+        await message.channel.send(todo_list)
+    
+    if message.content.startswith("/past"):
+        past_list = ""
+        try:
+            with open('done.txt', 'r') as f:
+                for i, line  in enumerate(f):
+                    past_list += str(i) + ". " + line
+        except FileNotFoundError:
+            print("No such file")
+        if not past_list:
+            past_list = "Nothing is completed"
+        await message.channel.send(past_list)
+    
+    if message.content.startswith("/addtd"):
+        todo = message.content[6:]
+        with open("todos.txt", "a") as f:
+            f.writelines("\n" + todo)
+        await message.channel.send("Todo added.")
+    
+    if message.content.startswith("/do"):
+        do = int(message.content[3:])
+        print(do)
+        lines = ""
+        try:
+            with open('todos.txt', 'r+') as f:
+                lines = f.readlines()
+        except FileNotFoundError:
+            print("No such file")
+            
+        if do < 0 or do > len(lines):
+            await message.channel.send("Task not in the list!")
+            
+        lines[do-1] = lines[do-1].strip() + " *Doing: " + message.author.name + "*"
+        
+        with open('todos.txt', 'r+') as f:
+            f.writelines(lines)
+        send_message = "Added *" + message.author.name + "* to task " + str(do)
+        await message.channel.send(send_message)
+        
+    if message.content.startswith("/complete"):
+        do = int(message.content[9:])
+        try:
+            with open('todos.txt', 'r+') as f:
+                lines = f.readlines()
+        except FileNotFoundError:
+            print("No such file")
+            
+        if do < 0 or do > len(lines):
+            await message.channel.send("Task not in the list!")
+            
+        done = lines[do-1].strip() + " *Completed: " + message.author.name + "*\n"
+        to_delete = lines[do-1].strip()
+        
+        with open('done.txt', 'a') as f:
+            f.writelines(done)
+        with open('todos.txt', 'w') as f:
+            for line in lines:
+                if line.strip("\n") != to_delete:
+                    print(line)
+                    f.write(line)
+            
+        send_message = "*" + message.author.name + "* did task " + str(do)
+        await message.channel.send(send_message)
 
 client.run(DISCORD_TOKEN)
