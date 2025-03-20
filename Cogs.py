dc77d5091119f392c711d68eccb6e374a656155c
NDSern <potarau123@gmail.com>
2025-03-20 15:02:11 +0200

    Rewrite everything

---
+++ Cogs.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import tasks, commands
+import datetime
+import bot_related_functions
+
+utc = datetime.timezone.utc
+leetcode_time = datetime.time(hour=0, minute=0, tzinfo=utc)
+coop_time = datetime.time(hour=15, minute=30, tzinfo=utc)
+
+LEETCODE_CHANNEL = 829060490079895622
+GENERAL_CHANNEL = 826136343021092897
+
+class DailyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.leetcode_daily.start()
+        self.coop_weekends_notice.start()
+        
+    def cog_unload(self):
+        self.leetcode_daily.cancel()
+        self.coop_weekends_notice.cancel()
+    
+    @tasks.loop(time=leetcode_time)
+    async def leetcode_daily(self):
+        channel = self.bot.get_channel(LEETCODE_CHANNEL)
+        if channel:
+            herf_daily = bot_related_functions.get_daily_leetcode_screenshot()
+            await channel.send(herf_daily)
+            await channel.send(file=discord.File('daily.png'))
+            print(f"Channel with ID {LEETCODE_CHANNEL} has been sent daily challenge")
+        else:
+            print(f"Channel with ID {LEETCODE_CHANNEL} not found.")
+            
+    @tasks.loop(time=coop_time)
+    async def coop_weekends_notice(self):
+        if datetime.datetime.now().weekday() < 5:
+            pass
+        
+        members = bot_related_functions.members_to_notify()
+        channel = self.bot.get_channel(GENERAL_CHANNEL)
+        
+        if channel:
+            notify_message = "It's time to Co-op "
+            for i in members:
+                notify_message += "<@" + str(i) + "> "
+            await channel.send(notify_message)
+            print("Notified")
+        else:
+            print("Coop channel to notify doesn't exist")
+        
--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import discord
+from discord.ext import commands
 import deepl
 import os
 from dotenv import load_dotenv
 import bot_related_functions
 import asyncio
 from datetime import datetime
+import random
+from Cogs import DailyCog
 
 start = datetime.now()
 
@@ -14,199 +17,75 @@ DEEPL_KEY = os.getenv('DEEPL_KEY')
 
 translator = deepl.Translator(DEEPL_KEY)
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
-@client.event
+RUN_ONCE = False
+async def run_once_on_startup():
+    if not RUN_ONCE:
+        try:
+            await add_cogs()
+            synced = await bot.tree.sync()
+            print("Commands synced: " + len(synced))
+        except Exception as e:
+            print(e)
+    RUN_ONCE = True
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(str(bot.user) + " is running!")
+    run_once_on_startup()
     
-    while not client.is_closed():
-        if bot_related_functions.new_day():
-            channel_id = bot_related_functions.get_subscribed_channel_id()
-            channel = client.get_channel(channel_id)
+@bot.event
+async def on_message(msg):
+    if msg.content == "!!!Sync":
+        RUN_ONCE = False
+        run_once_on_startup()
 
-            if channel:
-                herf_daily = bot_related_functions.get_daily_leetcode_screenshot()
-                await channel.send(herf_daily)
-                await channel.send(file=discord.File('daily.png'))
-                print(f"Channel with ID {channel_id} has been sent daily challenge")
-            else:
-                print(f"Channel with ID {channel_id} not found.")
-        
-        if bot_related_functions.notify_time():
-            members = bot_related_functions.members_to_notify()
-            channel_id = bot_related_functions.get_notify_channel(0)
-            channel = client.get_channel(channel_id)
-            
-            if channel:
-                notify_message = "It's time to Co-op "
-                for i in members:
-                    notify_message += "<@" + str(i) + "> "
-                await channel.send(notify_message)
-                print("Notified")
-            else:
-                print("Channel to notify doesn't exist")
-                
-        print("The bot is running, the time is: ", datetime.now()) 
-        
-        await asyncio.sleep(60)
+@bot.tree.command(description="Auto translate a phrase to English.")
+@discord.app_commands.describe(translate="need translated")
+async def translate(interaction: discord.Interaction, translate: str):
+    rawMessage = translate
+    rawMessage.strip()
+    translated_message = translator.translate_text(rawMessage, target_lang="EN-GB")
+    await interaction.response.send_message(translated_message)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('/clap_help'):
-        list_of_commands = "**/translate <msg>**: Translate message\n"
-        list_of_commands += "**/leetd**: Give a Leetcode daily problem screenshot\n"
-        list_of_commands += "**/leetnewd**: Get a new Leetcode daily screenshot, in case /leetd did not update for the new daily challenge\n"
-        list_of_commands += "**/leetsubscribe**: Subscribe channel to be notified everytime there is a new Leetcode daily\n"
-        await message.channel.send(list_of_commands)
+@bot.tree.command(description="Flip a coin. Head or Tail!")
+async def flip(interaction: discord.Interaction):
+    coin = random.randint(0, 1)
+    side = 'Error: Nothing is being randomized'
+    if coin == 0:
+        side = 'Head'
+    else:
+        side = 'Tail'
+    await interaction.response.send_message(side)
 
-    if message.content.startswith('/rand'):
-        return
+@bot.tree.command(description="Format: /rand <num1> or /rand <num1> <num2>")
+async def rand(interaction: discord.Integration, first: int , second: int | None):
+    if not second:
+        await interaction.response.send_message(str(random.randint(1, first)))
+    else:
+        await interaction.response.send_message(str(random.randint(first, second)))
 
-    if message.content.startswith('/translate'):
-        need_translation_message = message.content[10:]
-        translated_message = translator.translate_text(need_translation_message, target_lang="EN-GB")
-        await message.channel.send(translated_message.text)
-        
-    if message.content.startswith('/leetd'):
-        bot_related_functions.get_daily_leetcode_screenshot()
-        await message.channel.send(file=discord.File('daily.png'))
+@bot.tree.command(description="Get every members of the server.")
+async def getmembers(interaction: discord.Interaction):
+    guild = interaction.guild
+    members = ""
+    for i in guild.members:
+        members += "\n"
+        members += str(i.name)
+        members += ": "
+        members += str(i.id)
+    smessage = "There are " + str(guild.member_count) + ". They are: " + members
+    await interaction.response.send_message(smessage)
     
-    if message.content.startswith('/leetnewd'):
-        bot_related_functions.get_new_daily_leetcode_screenshot()
-        await message.channel.send(file=discord.File('daily.png'))
-    
-    if message.content.startswith('/leetsubscribe'):
-        subscribed_channel_id = message.channel.id
-        bot_related_functions.leetcode_daily_subscribed(subscribed_channel_id)
-        await message.channel.send("Channel subscribed")
-        
-    if message.content.startswith("/notifynow"):
-        members = bot_related_functions.members_to_notify()
-        channel_id = bot_related_functions.get_notify_channel(0)
-        channel = client.get_channel(channel_id)
-        
-        if channel:
-            notify_message = "It's time to Co-op "
-            for i in members:
-                notify_message += "<@" + str(i) + "> "
-            await channel.send(notify_message)
-            print("Notified")
-        else:
-            print("Channel to notify doesn't exist")
-            
-    if message.content.startswith("/getmembers"):
-        guild = message.channel.guild
-        members = ""
-        for i in guild.members:
-            members += "\n"
-            members += str(i.name)
-            members += ": "
-            members += str(i.id)
-            members += "\n"
-        smessage = "There are " + str(guild.member_count) + ". They are: " + members
-        await message.channel.send(smessage)
-        
-    #Game Jam stuffs
-    if message.content.startswith("/gamejam"):
-        gamejaminfo = "[Pirate Software Game Jam](<https://itch.io/jam/pirate>)\n"
-        gamejaminfo += "Theme to follow: Shadow & Alchemy\n"
-        gamejaminfo += "[Repo](<https://github.com/NDSern/Game-Jam-7-2024>)\n"
-        gamejaminfo += "[Docs](<https://drive.google.com/drive/folders/133YJRN0rvEuacpPovOMlSTYIQs4L3oKY?usp=sharing>)\n"
-        gamejaminfo += "Current project: Plague Doctor from DD have a shadow cloak and go melee people\n"
-        gamejaminfo += "Engine: Godot\n"
-        gamejaminfo += "Art & Sound software: Haven't decided yet\n"
-        await message.edit(suppress=True)
-        await message.channel.send(gamejaminfo)
-    
-    if message.content.startswith("/jam_help"):
-        commands = "**/todos**: Show the list of things to do\n"
-        commands += "**/past**: Show the list of things that have been done\n"
-        commands += "**/addtd <str>**: Add a new task\n"
-        commands += "**/do <int>**: Mark yourself as doing something\n"
-        commands += "**/complete <int>**: Mark yourself as completed a task\n"
-        await message.channel.send(commands)
-    
-    if message.content.startswith("/todos"):
-        todo_list = ""
-        try:
-            with open('todos.txt', 'r') as f:
-                for i, line  in enumerate(f):
-                    todo_list += str(i) + ". " + line
-        except FileNotFoundError:
-            print("No such file")
-        if not todo_list:
-            todo_list = "Everything is done, for now."
-        await message.channel.send(todo_list)
-    
-    if message.content.startswith("/past"):
-        past_list = ""
-        try:
-            with open('done.txt', 'r') as f:
-                for i, line  in enumerate(f):
-                    past_list += str(i) + ". " + line
-        except FileNotFoundError:
-            print("No such file")
-        if not past_list:
-            past_list = "Nothing is completed"
-        await message.channel.send(past_list)
-    
-    if message.content.startswith("/addtd"):
-        todo = message.content[7:]
-        with open("todos.txt", "a") as f:
-            f.writelines("\n" + todo)
-        await message.channel.send("Todo added.")
-    
-    if message.content.startswith("/do"):
-        do = int(message.content[4:])
-        print(do)
-        lines = ""
-        try:
-            with open('todos.txt', 'r+') as f:
-                lines = f.readlines()
-        except FileNotFoundError:
-            print("No such file")
-            
-        if do < 0 or do > len(lines):
-            await message.channel.send("Task not in the list!")
-            
-        lines[do-1] = lines[do-1].strip() + " *Doing: " + message.author.name + "*\n"
-        
-        with open('todos.txt', 'r+') as f:
-            f.writelines(lines)
-        send_message = "Added *" + message.author.name + "* to task " + str(do)
-        await message.channel.send(send_message)
-        
-    if message.content.startswith("/complete"):
-        do = int(message.content[9:])
-        try:
-            with open('todos.txt', 'r+') as f:
-                lines = f.readlines()
-        except FileNotFoundError:
-            print("No such file")
-            
-        if do < 0 or do > len(lines):
-            await message.channel.send("Task not in the list!")
-            
-        done = lines[do-1].strip() + " *Completed: " + message.author.name + "*\n"
-        to_delete = lines[do-1].strip()
-        
-        with open('done.txt', 'a') as f:
-            f.writelines(done)
-        with open('todos.txt', 'w') as f:
-            for line in lines:
-                if line.strip("\n") != to_delete:
-                    print(line)
-                    f.write(line)
-            
-        send_message = "*" + message.author.name + "* did task " + str(do)
-        await message.channel.send(send_message)
 
-client.run(DISCORD_TOKEN)
+async def add_cogs():
+    await bot.add_cog(DailyCog(bot))
+    print("Cog added")
+
+    
+bot.run(DISCORD_TOKEN)
